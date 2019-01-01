2b63385f508e23ae760e064ec381a87fe51cfe1e
xaviguasch <xaviguasch@gmail.com>
2019-01-01 14:25:11 +0100

    playing with route params

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.get('/dog', function(req, res) {
     res.send('MEOW!!!!!')
 })
 
+app.get('/r/:subredditName', function(req, res) {    
+    res.send('Welcome to a subreddit!')
+})
+
+app.get('*', function(req, res) {    
+    res.send('You are a star!!!')
+})
+
+
 app.listen(3000, 'localhost', function() {
     console.log('server has started, listening on port 3000');
 })
